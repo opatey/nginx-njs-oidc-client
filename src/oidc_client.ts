71c13d17acf360d4964c130ba7474d7ee6a07bc5
Owen Patey <owen_patey@hotmail.com>
2020-11-20 16:03:37 +0000

    auth_request from inside sample app

--- src/oidc_client.ts
+++ src/oidc_client.ts
@@ -1,13 +1,16 @@
 /// <reference path="../node_modules/njs-types/ngx_http_js_module.d.ts" />
 
+function handler(request: NginxHTTPRequest) {
+  request.error("OIDC handler called: " + request.uri);
+  
+  request.return(401, request.uri);
+}
+
 function initiate(request: NginxHTTPRequest) {
-  request.headersOut['Location'] = 'https://bbc.co.uk';
-  request.return(302);
+  request.error("OIDC initiate called: " + request.uri);
+
+  request.return(302, "https://bbc.co.uk");
 }
 
-function auth_handler(request: NginxHTTPRequest) {
-  request.return(401);
-}
-
-export default { initiate, auth_handler }
+export default { handler, initiate }
 
