19fa960ecfcd1e980d3fd37ff6dda14466b092e6
Owen Patey <owen_patey@hotmail.com>
2020-11-22 19:09:28 +0000

    exchange authorization code for id_token

--- src/oidc_client.ts
+++ src/oidc_client.ts
@@ -1,10 +1,35 @@
 /// <reference path="../node_modules/njs-types/ngx_http_js_module.d.ts" />
 
 function handler(request: NginxHTTPRequest) {
+  const client_id          = request.variables['oidc_client_id'];
+  const client_secret      = request.variables['oidc_client_secret'];
+
   request.error("OIDC handler called: " + request.uri);
 
-  if (request.uri.indexOf("code=") > 0) {
-    // exchange with IDP, set session and redirect
+  if (request.args.code) {
+    var token_request_body = "grant_type=authorization_code"
+      + "&code=" + request.args.code
+      + "&client_id=" + client_id
+      + "&client_secret=" + client_secret
+      + "&redirect_uri=http://localhost:8080/oidc"
+
+    request
+      .subrequest("/oidc/token", {
+        method: "POST",
+        body: token_request_body
+      })
+      .then(tokens_response => {
+        request.error("tokens response: " + JSON.stringify(tokens_response));
+        return JSON.parse(tokens_response.responseBody ?? "{}");
+      })
+      .then(tokens => {
+          const id_token = tokens['id_token'];
+          if (!id_token) {
+              throw new Error("id_token is not available");
+          }
+          request.return(200, id_token);
+      })
+      .catch(reason => request.return(500, reason));
   } else {
     request.return(401, request.uri);
   }  
@@ -12,11 +37,14 @@ function handler(request: NginxHTTPRequest) {
 
 function initiate(request: NginxHTTPRequest) {
   request.error("OIDC initiate called: " + request.uri);
+    
+  const client_id          = request.variables['oidc_client_id'];
+  const authorize_endpoint = request.variables['oidc_auth_endpoint'];
 
   request.return(302,
     // source from configuration
-    "https://login.microsoftonline.com/94db9e7d-6399-47cb-87ba-f83853e100d0/oauth2/v2.0/authorize"
-    + "?client_id=b40f9fdf-73b8-4480-9723-6bc01c2e6449"
+    authorize_endpoint
+    + "?client_id=" + client_id
     + "&redirect_uri=http://localhost:8080/oidc"
     + "&scope=openid"
 
